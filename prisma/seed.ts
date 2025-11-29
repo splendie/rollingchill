a0a4ae2a15dc6c4d9f55ab200c93822fadad11ad
Indri Yunita <indri.yunita@tutanota.com>
2025-11-29 14:49:22 +0700

    Fix Prisma build 2

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma = new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 const products = [
   {
